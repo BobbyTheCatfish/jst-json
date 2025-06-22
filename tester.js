db0189256e901cd25218ea5fe3dd4c4e86c6eaa7
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2025-06-22 00:02:45 -0400

    testing program to ensure i got everything right

---
+++ tester.js
@@ -0,0 +1,68 @@
+// @ts-check
+/**
+ * @typedef {Record<string, Record<string, Record< string, string>>>} Book
+ */
+/** @type {Book} */
+const ot = require("./jst/old-testament.json");
+/** @type {Book} */
+const nt = require("./jst/new-testament.json");
+/** @type {Record<string, Record<string, (string[] | null | undefined)[]>>} */
+const jst = require("./jst/jst-reference.json");
+
+const jstCopy = { ...jst };
+
+/**
+ * @template {keyof jst} T
+ * @param {T} book 
+ * @param {keyof jst[T]} chapter 
+ * @param {number} index
+ */
+function getJST(book, chapter, index) {
+    const verse = jstCopy[book][chapter][index]
+    if (verse === undefined) console.log(`${book} ${chapter} [${index}] doesn't exist.`);
+    if (verse === null) console.log(`${book} ${chapter} [${index}] already used!`);
+
+    jstCopy[book][chapter][index] = null;
+    return verse;
+}
+
+const jstMatch = /\[JST ([0-9]{1,2})\]/
+
+/**
+ * @param {typeof ot | typeof nt} json 
+ */
+function analyzeTestament(json) {
+    for (const bookName in json) {
+        const book = json[bookName];
+
+        for (const chapterNumber in book) {
+
+            const chapter = book[chapterNumber];
+            for (const verseNumber in chapter) {
+                const verse = chapter[verseNumber];
+                const match = jstMatch.exec(verse);
+                if (!match) continue;
+
+                const index = parseInt(match[1]);
+                getJST(bookName, chapterNumber, index);
+            }
+
+        }
+
+    }
+}
+
+analyzeTestament(ot)
+analyzeTestament(nt)
+
+for (const bookName in jst) {
+    const book = jst[bookName]
+    for (const chapterNumber in book) {
+        const chapter = book[chapterNumber];
+        const unused = [];
+        chapter.forEach((v, i) => {
+            if (v !== null) unused.push(`${bookName} ${chapterNumber} [${i}] is unused.`)
+        });
+        if (unused.length > 0) console.log(unused.join("\n"));
+    }
+}
